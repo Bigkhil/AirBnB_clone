1cacde49f58c01680b00c6b97ccc58817d11fee5
Mohamed khalil <khm40005000@outlook.com>
2024-05-20 02:51:28 +0300

    some changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 '''this is the base model for all the classes'''
+import uuid
+import datetime
+
+
+class BaseModel():
+    '''this is the base class for all classes'''
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.created_at = datetime.date()
