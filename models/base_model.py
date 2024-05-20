be7a939a31ed7ea523d5791dd4c2a1563f9dbf4f
Mohamed khalil <khm40005000@outlook.com>
2024-05-20 04:00:09 +0300

    some changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 '''this is the base model for all the classes'''
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel():
     '''this is the base class for all classes'''
     def __init__(self):
-        self.id = uuid.uuid4()
-        self.created_at = datetime.date()
+        '''constructor for class instances'''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''returns string to display when printing the instances'''
+        return ("[{}] ({}) {}".
+                format(BaseModel.__name__, self.id, self.__dict__))
+
+    def save(self):
+        '''updates the public instance attribute updated_at'''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict = self.__dict__
+        dict['__class__'] = BaseModel.__name__
+        dict['created_at'] = dict['updated_at'] = self.created_at.isoformat()
+        return dict
