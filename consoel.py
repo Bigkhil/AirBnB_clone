d27bab7c9eb7a1779449797463e365428a6a43b2
Mohamed khalil <khm40005000@outlook.com>
2024-05-21 04:02:03 +0300

    some changes

--- consoel.py
+++ consoel.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 '''this is the entry point to our console'''
-from cmd import Cmd
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    '''HBNB console for the webserver'''
+    prompt = "(hbnb)"
+    def do_quit(self):
+        '''quit the interpreter'''
+        return True
+    
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
