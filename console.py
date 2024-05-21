88b68c517a75bc9e508fdc04fb006d93f31e8358
Mohamed khalil <khm40005000@outlook.com>
2024-05-21 04:11:02 +0300

    some changes

--- console.py
+++ console.py
@@ -5,11 +5,17 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     '''HBNB console for the webserver'''
     prompt = "(hbnb)"
-    def do_quit(self):
+    def do_quit(self, line):
         '''quit the interpreter'''
         return True
-    
 
+    def do_EOF(self, line):
+        '''handle EOF'''
+        if line == '':
+            return True
+    def emptyline(self):
+        '''ignore empty line'''
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
