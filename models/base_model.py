80d615bf8425c4cf2c4ed11fee46ab52b71b2739
Mohamed khalil <khm40005000@outlook.com>
2024-06-15 02:22:34 +0300

    some changes

--- models/base_model.py
+++ models/base_model.py
@@ -18,10 +18,10 @@ class BaseModel():
                 else:
                     setattr(self, key, value)
         else:
-            storage.new()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         '''returns string to display when printing the instances'''
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,28 +3,29 @@
 import os
 import json
 
+
 class FileStorage():
-	'''file_storage class used for serialization and deserialization'''
-	__file_path = './file.json'
-	__objects = {}
+    '''file_storage class used for serialization and deserialization'''
+    __file_path = './file.json'
+    __objects = {}
 
-	@property
-	def all(self):
-		'''return __objects dictionary'''
-		return self.__objects
-	
-	def new(self, obj):
-		'''sets in __objects the obj with key <obj class name>.id'''
-		key = '{}.{}'.format(obj.__name__, obj.id)
-		self.__objects[key] = obj.to_dict()
 
-	def save(self):
-		'''serializes __objects to the JSON file (path: __file_path)'''
-		with open(self.__file_path, 'w') as file:
-			file.write(json.dumps(self.__objects))
+    def all(self):
+        '''return __objects dictionary'''
+        return self.__objects
 
-	def reload(self):
-		''' deserializes the JSON file to __objects'''
-		if os.path.exists(self.__file_path):
-			with open(self.__file_path, 'r') as file:
-				self.__objects = json.loads(file.read())
+    def new(self, obj):
+        '''sets in __objects the obj with key <obj class name>.id'''
+        key = '{}.{}'.format(obj, obj.id)
+        self.__objects[key] = obj.to_dict()
+
+    def save(self):
+        '''serializes __objects to the JSON file (path: __file_path)'''
+        with open(self.__file_path, 'w') as file:
+            file.write(json.dumps(self.__objects))
+
+    def reload(self):
+        ''' deserializes the JSON file to __objects'''
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                self.__objects = json.loads(file.read())
